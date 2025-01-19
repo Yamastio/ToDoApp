271fc31de769c2242eeb893596c941c11c193909
Yamastio <mikuchan312314@gmail.com>
2025-01-20 06:10:00 +0700

    Add: MongoDB.kt

--- composeApp/src/commonMain/kotlin/data/MongoDB.kt
+++ composeApp/src/commonMain/kotlin/data/MongoDB.kt
@@ -1,4 +1,21 @@
 package data
 
+import domain.ToDoTask
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+
 class MongoDB {
+    var realm: Realm? = null
+
+    private fun configureTheRealm() {
+        if (realm == null || realm!!.isClosed()) {
+            val config = RealmConfiguration.Builder(
+                schema = setOf(ToDoTask::class)
+            )
+                .compactOnLaunch()
+                .build()
+            realm = Realm.open(config)
+        }
+    }
+
 }
