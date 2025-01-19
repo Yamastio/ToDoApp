3fa4b6ac58df69e4982131df75e03e2cbf535c26
Yamastio <mikuchan312314@gmail.com>
2025-01-20 06:08:17 +0700

    Add: HomeScreen.kt

--- composeApp/src/commonMain/kotlin/presentation/screen/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screen/home/HomeScreen.kt
@@ -35,37 +35,47 @@ import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import domain.RequestState
 import domain.ToDoTask
+import presentation.components.ErrorScreen
+import presentation.components.LoadingScreen
+import presentation.components.TaskView
 import presentation.screen.task.TaskScreen
 
 class HomeScreen : Screen {
     @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
-        Scaffold(
-            topBar = {
-                CenterAlignedTopAppBar(title = { Text(text = "Home") })
-            },
-            floatingActionButton = {
-                FloatingActionButton(
-                    onClick = { },
-                    shape = RoundedCornerShape(size = 12.dp)
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Edit,
-                        contentDescription = "Edit Icon"
-                    )
-                }
-            }
-        ) { padding ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 24.dp)
-                    .padding(
-                        top = padding.calculateTopPadding(),
-                        bottom = padding.calculateBottomPadding()
-                    )
+        Scaffold(topBar = {
+            CenterAlignedTopAppBar(title = { Text(text = "Home") })
+        }, floatingActionButton = {
+            FloatingActionButton(
+                onClick = { }, shape = RoundedCornerShape(size = 12.dp)
             ) {
+                Icon(
+                    imageVector = Icons.Default.Edit, contentDescription = "Edit Icon"
+                )
+            }
+        }) { padding ->
+            Column(
+                modifier = Modifier.fillMaxSize().padding(top = 24.dp).padding(
+                    top = padding.calculateTopPadding(),
+                    bottom = padding.calculateBottomPadding()
+                )
+            ) {
+                DisplayTask(
+                    modifier = Modifier.weight(1f),
+                    tasks = RequestState.Idle,
+                    onSelect = { selectedTask -> },
+                    onFavorite = { task, isFavorite -> },
+                    onComplete = { task, completed -> }
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                DisplayTask(
+                    modifier = Modifier.weight(1f),
+                    tasks = RequestState.Idle,
+                    showActive = false,
+                    onComplete = { task, completed ->},
+                    onDelete = {task ->}
+                )
 
 
             }
@@ -76,49 +86,89 @@ class HomeScreen : Screen {
 @Composable
 fun DisplayTask(
     modifier: Modifier = Modifier,
-    task: RequestState<List<ToDoTask>>,
+    tasks: RequestState<List<ToDoTask>>,
     showActive: Boolean = true,
     onSelect: ((ToDoTask) -> Unit)? = null,
     onFavorite: ((ToDoTask, Boolean) -> Unit)? = null,
     onComplete: (ToDoTask, Boolean) -> Unit,
     onDelete: ((ToDoTask) -> Unit)? = null,
-){
+) {
     var showDialog by remember { mutableStateOf(false) }
     var taskToDelete: ToDoTask? by remember { mutableStateOf(null) }
 
     if (showDialog) {
-        AlertDialog(
-            title = {
-                Text(text = "Delete", fontSize = MaterialTheme.typography.titleLarge.fontSize)
-            },
-            text = {
-                Text(
-                    text = "Are you sure you want to remove '${taskToDelete!!.title}' task?",
-                    fontSize = MaterialTheme.typography.bodyMedium.fontSize
-                )
-            },
-            confirmButton = {
-                Button(onClick = {
-                    onDelete?.invoke(taskToDelete!!)
-                    showDialog = false
-                    taskToDelete = null
-                }) {
-                    Text(text = "Yes")
-                }
-            },
-            dismissButton = {
-                TextButton(
-                    onClick = {
-                        taskToDelete = null
-                        showDialog = false
-                    }
-                ) {
-                    Text(text = "Cancel")
-                }
-            },
-            onDismissRequest = {
+        AlertDialog(title = {
+            Text(text = "Delete", fontSize = MaterialTheme.typography.titleLarge.fontSize)
+        }, text = {
+            Text(
+                text = "Are you sure you want to remove '${taskToDelete!!.title}' task?",
+                fontSize = MaterialTheme.typography.bodyMedium.fontSize
+            )
+        }, confirmButton = {
+            Button(onClick = {
+                onDelete?.invoke(taskToDelete!!)
+                showDialog = false
+                taskToDelete = null
+            }) {
+                Text(text = "Yes")
+            }
+        }, dismissButton = {
+            TextButton(onClick = {
                 taskToDelete = null
                 showDialog = false
+            }) {
+                Text(text = "Cancel")
+            }
+        }, onDismissRequest = {
+            taskToDelete = null
+            showDialog = false
+        })
+    }
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            modifier = Modifier.padding(horizontal = 12.dp),
+            text = if (showActive) "Active Tasks" else "Completed Tasks",
+            fontSize = MaterialTheme.typography.titleMedium.fontSize,
+            fontWeight = FontWeight.Medium
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        tasks.DisplayResult(
+            onLoading = { LoadingScreen() },
+            onError = { ErrorScreen(message = it) },
+            onSuccess = {
+                if (it.isNotEmpty()) {
+                    LazyColumn(modifier = Modifier.padding(horizontal = 24.dp)) {
+                        items(
+                            items = it,
+                            key = { task -> task._id }
+                        ) { task ->
+                            TaskView(
+                                showActive = showActive,
+                                task = task,
+                                onSelect = { onSelect?.invoke(it) },
+                                onComplete = { selectedTask, completed ->
+                                    onComplete(
+                                        selectedTask,
+                                        completed
+                                    )
+                                },
+                                onFavorite = { selectedTask, favorite ->
+                                    onFavorite?.invoke(
+                                        selectedTask,
+                                        favorite
+                                    )
+                                },
+                                onDelete = { selectedTask ->
+                                    taskToDelete = selectedTask
+                                    showDialog = true
+                                }
+                            )
+                        }
+                    }
+
+                } else {
+                    ErrorScreen()
+                }
             }
         )
     }
